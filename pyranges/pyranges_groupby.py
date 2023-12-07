992dc9753a0cd9253714182a2043dd2b1337a4f7
endre bakken stovner <endrebakkenstovner@endres-MacBook-Air.local>
2023-12-07 12:15:00 +0100

    GroupBy returns PyRangesGroupBy

--- pyranges/pyranges_groupby.py
+++ pyranges/pyranges_groupby.py
@@ -19,16 +19,204 @@ def return_pyranges_if_possible(method):
 
 
 class PyRangesGroupBy(pd.core.groupby.DataFrameGroupBy):
+
     @return_pyranges_if_possible
     def agg(self, *args, **kwargs):
         return super().agg(*args, **kwargs)
 
     @return_pyranges_if_possible
-    def aggregate(self, func, *args, **kwargs):
-        return super().aggregate(func, *args, **kwargs)
+    def aggregate(self, *args, **kwargs):
+        return super().aggregate(*args, **kwargs)
 
     @return_pyranges_if_possible
-    def apply(self, func, *args, **kwargs):
-        return super().apply(func, *args, **kwargs)
+    def all(self, *args, **kwargs):
+        return super().all(*args, **kwargs)
 
-    # Apply the decorator to other methods as needed
+    @return_pyranges_if_possible
+    def any(self, *args, **kwargs):
+        return super().any(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def apply(self, *args, **kwargs):
+        return super().apply(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def bfill(self, *args, **kwargs):
+        return super().bfill(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def cumcount(self, *args, **kwargs):
+        return super().cumcount(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def cummax(self, *args, **kwargs):
+        return super().cummax(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def cummin(self, *args, **kwargs):
+        return super().cummin(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def cumprod(self, *args, **kwargs):
+        return super().cumprod(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def cumsum(self, *args, **kwargs):
+        return super().cumsum(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def describe(self, *args, **kwargs):
+        return super().describe(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def diff(self, *args, **kwargs):
+        return super().diff(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def ewm(self, *args, **kwargs):
+        return super().ewm(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def expanding(self, *args, **kwargs):
+        return super().expanding(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def ffill(self, *args, **kwargs):
+        return super().ffill(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def fillna(self, *args, **kwargs):
+        return super().fillna(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def filter(self, *args, **kwargs):
+        return super().filter(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def first(self, *args, **kwargs):
+        return super().first(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def get_group(self, *args, **kwargs):
+        return super().get_group(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def head(self, *args, **kwargs):
+        return super().head(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def idxmax(self, *args, **kwargs):
+        return super().idxmax(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def idxmin(self, *args, **kwargs):
+        return super().idxmin(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def last(self, *args, **kwargs):
+        return super().last(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def max(self, *args, **kwargs):
+        return super().max(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def mean(self, *args, **kwargs):
+        return super().mean(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def median(self, *args, **kwargs):
+        return super().median(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def min(self, *args, **kwargs):
+        return super().min(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def ngroup(self, *args, **kwargs):
+        return super().ngroup(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def nunique(self, *args, **kwargs):
+        return super().nunique(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def ohlc(self, *args, **kwargs):
+        return super().ohlc(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def pct_change(self, *args, **kwargs):
+        return super().pct_change(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def pipe(self, *args, **kwargs):
+        return super().pipe(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def prod(self, *args, **kwargs):
+        return super().prod(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def quantile(self, *args, **kwargs):
+        return super().quantile(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def rank(self, *args, **kwargs):
+        return super().rank(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def resample(self, *args, **kwargs):
+        return super().resample(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def rolling(self, *args, **kwargs):
+        return super().rolling(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def sample(self, *args, **kwargs):
+        return super().sample(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def sem(self, *args, **kwargs):
+        return super().sem(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def shift(self, *args, **kwargs):
+        return super().shift(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def size(self, *args, **kwargs):
+        return super().size(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def skew(self, *args, **kwargs):
+        return super().skew(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def std(self, *args, **kwargs):
+        return super().std(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def sum(self, *args, **kwargs):
+        return super().sum(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def tail(self, *args, **kwargs):
+        return super().tail(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def take(self, *args, **kwargs):
+        return super().take(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def transform(self, *args, **kwargs):
+        return super().transform(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def value_counts(self, *args, **kwargs):
+        return super().value_counts(*args, **kwargs)
+
+    @return_pyranges_if_possible
+    def var(self, *args, **kwargs):
+        return super().var(*args, **kwargs)
+
--- tests/unit/test_pandas_overrides.py
+++ tests/unit/test_pandas_overrides.py
@@ -1,6 +1,7 @@
 """Test that overriden pandas methods work and return PyRanges objects."""
 
 import pandas as pd
+import pytest
 
 import pyranges as pr
 from pyranges import PyRanges
@@ -25,6 +26,19 @@ def test_loc_get():
     assert isinstance(res, pr.PyRanges)
 
 
+@pytest.fixture
+def gr():
+    return pr.PyRanges(
+        {
+            "Chromosome": ["chr1"] * 2,
+            "Start": [0, 10],
+            "End": [40, 20],
+            "Gene": ["DonkeyKong", "Mario"],
+            "Val": [50, 30]
+        }
+    )
+
+
 def test_loc_set():
     gr = pr.PyRanges({"Chromosome": ["chr1"], "Start": [0], "End": [40]})
 
@@ -33,12 +47,161 @@ def test_loc_set():
     assert gr.Start[0] == 1
 
 
-def test_groupby():
-    gr = pr.PyRanges({"Chromosome": ["chr1", "chr1", "chr2"], "Start": [4, 10, 4], "End": [40, 11, 10]})
+def test_groupby_agg(gr):
     g = gr.groupby("Chromosome")
-    assert isinstance(g, PyRangesGroupBy)
-    res = g.agg({"Chromosome": "first", "Start": "min", "End": "max"})
-    assert isinstance(res, PyRanges)
+    assert isinstance(g.agg("first"), pd.DataFrame)
 
-    assert res.Start.tolist() == [4, 4]
-    assert res.End.tolist() == [40, 10]
+    g = gr.groupby("Gene")
+    assert isinstance(g.agg("first"), pr.PyRanges)
+
+
+def test_groupby_aggregate(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.aggregate("first"), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.aggregate("first"), pr.PyRanges)
+
+
+def test_groupby_all(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.all(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.all(), pr.PyRanges)
+
+
+def test_groupby_any(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.any(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.any(), pr.PyRanges)
+
+
+def test_groupby_apply(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.apply(lambda df: df.head()), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.apply(lambda df: df.head()), pr.PyRanges)
+
+
+def test_groupby_bfill(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.bfill(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.bfill(), pr.PyRanges)
+
+
+def test_groupby_ffill(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.ffill(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.ffill(), pr.PyRanges)
+
+
+def test_groupby_fillna(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.fillna(0), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.fillna(0), pr.PyRanges)
+
+
+def test_groupby_filter(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.filter(lambda df: True), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.filter(lambda df: True), pr.PyRanges)
+
+
+def test_groupby_first(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.first(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.first(), pr.PyRanges)
+
+
+def test_groupby_get_group(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.get_group("chr1"), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.get_group("Mario"), pr.PyRanges)
+
+
+def test_groupby_head(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.head(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.head(), pr.PyRanges)
+
+
+def test_groupby_last(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.last(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.last(), pr.PyRanges)
+
+
+def test_groupby_min(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.min(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.min(), pr.PyRanges)
+
+
+def test_groupby_pipe(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.pipe(lambda df: df).agg("first"), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.pipe(lambda df: df).agg("first"), pr.PyRanges)
+
+
+def test_groupby_prod(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.prod("first"), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.prod(), pr.PyRanges)
+
+
+def test_groupby_sample(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.sample(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.sample(), pr.PyRanges)
+
+
+def test_groupby_sum(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.sum(), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.sum(), pr.PyRanges)
+
+
+def test_groupby_tail(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.tail(1), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.tail(1), pr.PyRanges)
+
+
+def test_groupby_transform(gr):
+    g = gr.groupby("Chromosome")
+    assert isinstance(g.transform("first"), pd.DataFrame)
+
+    g = gr.groupby("Gene")
+    assert isinstance(g.transform("first"), pr.PyRanges)
