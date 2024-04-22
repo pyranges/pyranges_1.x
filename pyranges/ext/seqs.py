c18a2ff04520019e6312ad583db24b785a38d78f
marco-mariotti <marco.mariotti@ub.edu>
2024-04-22 15:38:20 +0200

    translate and reverse complement accept series

--- pyranges/ext/seqs.py
+++ pyranges/ext/seqs.py
@@ -1,4 +1,7 @@
 import itertools
+from collections.abc import Iterable
+
+import pandas as pd
 
 import pyranges as pr  # noqa: F401
 
@@ -47,7 +50,12 @@ for gc_code in GENETIC_CODE_AAS:
     GENETIC_CODES[gc_code + "+U"]["TGA"] = "U"
 
 
-def reverse_complement(seq: str, *, is_rna: bool = False, check: bool = False) -> str:
+def reverse_complement(
+    seq: str | pd.Series | Iterable[str],
+    *,
+    is_rna: bool = False,
+    check: bool = False,
+) -> str | pd.Series:
     """Reverse complement a DNA sequence.
 
     The input sequence is in DNA alphabet, upper or lowercase (ATGCatgc). The case is maintained in output.
@@ -55,8 +63,8 @@ def reverse_complement(seq: str, *, is_rna: bool = False, check: bool = False) -
 
     Parameters
     ----------
-    seq : str
-        nucleotide sequence in DNA format (characters: ATGCatgc)
+    seq : str | Iterable[str]
+        nucleotide sequence in DNA format (characters: ATGCatgc), or an iterable of such sequences, e.g. a Series
 
     is_rna : bool
         use this to provide the input seq in RNA format instead (characters: AUGCaugc)
@@ -67,7 +75,8 @@ def reverse_complement(seq: str, *, is_rna: bool = False, check: bool = False) -
     Returns
     -------
     revcompseq : str
-        reverse complement nucleotide sequence in DNA format (or RNA if is_rna was set to True)
+        Reverse complement nucleotide sequence in DNA format (or RNA if is_rna was set to True).
+        If an iterable was provided, a Series of reverse complement sequences is returned.
 
     Examples
     --------
@@ -88,25 +97,64 @@ def reverse_complement(seq: str, *, is_rna: bool = False, check: bool = False) -
     ...
     ValueError: One or more characters in the input string are not present in the translation table.
 
+    >>> some_seqs=["ATGAAATTTGGGTGA", "AAAGAAATGGGTGACCCCC"]
+    >>> pr.seqs.reverse_complement(some_seqs)
+    0        TCACCCAAATTTCAT
+    1    GGGGGTCACCCATTTCTTT
+    dtype: object
+
+    If a Series is provided, the output Series preserve its index:
+
+    >>> some_seqs = pd.Series(["ATGAAATTTGGGTGA", "AAAGAAATGGGTGACCCCC"], index=["s1", "s2"])
+    >>> pr.seqs.reverse_complement(some_seqs)
+    s1        TCACCCAAATTTCAT
+    s2    GGGGGTCACCCATTTCTTT
+    dtype: object
+
     """
+
+    def _rev_comp(seq: str, transtable: dict, *, check: bool) -> str:
+        if check:
+            accepted_chars = {chr(charint) for charint in transtable}
+            if not set(seq).issubset(accepted_chars):
+                msg = "One or more characters in the input string are not present in the translation table."
+                raise ValueError(msg)
+        return seq.translate(transtable)[::-1]
+
+    def _vectorized_rev_comp(seqs: Iterable[str], transtable: dict, *, check: bool) -> pd.Series:
+        seqs = pd.Series(list(seqs)) if not isinstance(seqs, pd.Series) else seqs
+        if check:
+            accepted_chars = {chr(charint) for charint in transtable}
+            wrong_seq_sel = seqs.str.contains(f"[^{accepted_chars}]")
+            if wrong_seq_sel.any():
+                example = str(seqs[wrong_seq_sel].head(1)).split("\n")[0]
+                msg = (
+                    f"One or more characters in {wrong_seq_sel.sum()} input string(s) are not present in the "
+                    f"translation table, such as: {example}"
+                )
+                raise ValueError(msg)
+
+        return seqs.str.translate(transtable).str[::-1]
+
     transtable = complement if not is_rna else rnacomplement
-    if check and not all(char in transtable for char in set(seq)):
-        msg = "One or more characters in the input string are not present in the translation table."
-        raise ValueError(msg)
-    return seq.translate(transtable)[::-1] if not is_rna else seq.translate(transtable)[::-1]
+    return (
+        _rev_comp(seq, transtable, check=check)
+        if isinstance(seq, str)
+        else _vectorized_rev_comp(seq, transtable, check=check)
+    )
 
 
 GENETIC_CODE_OPTION = str | int | dict
 
 
-def translate(
-    seq: str,
+def translate(  # noqa: C901
+    seq: str | pd.Series | Iterable[str],
     *,
     genetic_code: GENETIC_CODE_OPTION = "1",
     unknown: str = "X",
     sanitize: bool = True,
     cache: int | bool = False,
-) -> str:
+) -> str | pd.Series:
     """Translate a coding sequence into protein.
 
     The input sequence is expected to be a DNA sequence in uppercase format (characters: ATGC).
@@ -114,8 +162,8 @@ def translate(
 
     Parameters
     ----------
-    seq : str
-        nucleotide sequence in uppercase DNA format (characters: ATGC)
+    seq : str | Iterable[str]
+        nucleotide sequence in uppercase DNA format (characters: ATGC), or an iterable of such sequences, e.g. a Series
 
     genetic_code : int | str | dict
         int or string-converted NCBI index for genetic code (https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi)
@@ -142,7 +190,8 @@ def translate(
     Returns
     -------
     pep : str
-        protein sequence resulting from translation, with gaps as '-' and unknown characters as 'X'
+        Protein sequence resulting from translation, with gaps as '-' and unknown characters as 'X'.
+        If an iterable was provided, a Series of protein sequences is returned.
 
     Examples
     --------
@@ -171,6 +220,25 @@ def translate(
     ...
     ValueError: translate ERROR cannot find codon AUG (pos 0-3) in genetic_code!
 
+    Output for list of sequences is a Series:
+
+    >>> pr.seqs.translate(['ACTGCATAA', 'ATGGGGTACTAG'])
+    0     TA*
+    1    MGY*
+    dtype: object
+
+    >>> pr.seqs.translate(['AAUUUtACTGCACTACGACTAGCTAC', 'ACACTGACTGACTATCTGATCGAC'], sanitize=True)
+    0    NFTALRLAX
+    1     TLTDYLID
+    dtype: object
+
+    When the input is a Series, the output is a Series with the same index:
+
+    >>> x = pd.Series(['ACTGCATAA', 'ATGGGGTACTAG'], index=['s1', 's2'])
+    >>> pr.seqs.translate(x)
+    s1     TA*
+    s2    MGY*
+    dtype: object
 
     Caching makes sense when translating many sequences.
     For large enough data, translate with ``cache=True`` is 3x faster than non-caching translate:
@@ -178,24 +246,15 @@ def translate(
     >>> import random
     >>> random.seed(42)
     >>> many_seqs = [ "".join(random.choices("ATGC", k=1000)) for _ in range(100000) ]
-    >>> translated_seqs = [pr.seqs.translate(seq, cache=True) for seq in many_seqs]
+    >>> translated_seqs = pr.seqs.translate(many_seqs, cache=True)
     >>> translated_seqs[0][:50]
     'DRHKKEHVLTRLRGINVIGDRANYLS*CYESYRQGDS*PGRDTLPYHV*D'
 
     """
-    if isinstance(genetic_code, dict):
-        codon_table = genetic_code
-    elif str(genetic_code) in GENETIC_CODES:
-        codon_table = GENETIC_CODES[str(genetic_code)]
-    else:
-        msg = f"translate ERROR genetic_code input not recognized: {genetic_code}"
-        raise ValueError(msg)
 
-    if sanitize:
-        seq = seq.upper().replace("U", "T")
-
-    output = []
-    if not cache:
+    def _translate_noncached(seq: str, *, sanitize: bool = False) -> str:
+        seq = seq.upper().replace("U", "T") if sanitize else seq
+        output = []
         for pos in range(0, len(seq), 3):
             codon = seq[pos : pos + 3]
             if codon in codon_table:
@@ -205,20 +264,62 @@ def translate(
             else:
                 msg = f"translate ERROR cannot find codon {codon} (pos {pos}-{pos+3}) in genetic_code!"
                 raise ValueError(msg)
-    else:
-        cache = 3 if cache is True else cache
-        kmer_codon_table = _get_kmer_codon_table(genetic_code, cache)
-        step = cache * 3
+        return "".join(output)
+
+    def _translate_cached(
+        seq: str,
+        *,
+        sanitize: bool = False,
+    ) -> str:
+        seq = seq.upper().replace("U", "T") if sanitize else seq
+        output = []
         for pos in range(0, len(seq), step):
             multicodon = seq[pos : pos + step]
             output.append(
                 kmer_codon_table[multicodon]
                 if multicodon in kmer_codon_table
                 # for final bits of sequences which are shorter than k codons, and also for sequences containing Ns:
-                else translate(multicodon, genetic_code=genetic_code, unknown=unknown, cache=False),
+                else _translate_noncached(multicodon, sanitize=False),
             )
+        return "".join(output)
 
-    return "".join(output)
+    def _vectorized_translate(
+        seqs: Iterable[str],
+        *,
+        sanitize: bool,
+    ) -> pd.Series:
+        seqs = pd.Series(list(seqs)) if not isinstance(seqs, pd.Series) else seqs
+        seqs = seqs.str.upper().str.replace("U", "T") if sanitize else seqs
+
+        return (
+            seqs.apply(_translate_noncached)
+            if not cache  # above: no cache
+            else seqs.apply(_translate_cached)
+        )
+
+    if isinstance(genetic_code, dict):
+        codon_table = genetic_code
+    elif str(genetic_code) in GENETIC_CODES:
+        codon_table = GENETIC_CODES[str(genetic_code)]
+    else:
+        msg = f"translate ERROR genetic_code input not recognized: {genetic_code}"
+        raise ValueError(msg)
+
+    if cache:
+        # precomputing all possible translations for k codons, if not computed already (see _get_kmer_codon_table)
+        cache = 3 if cache is True else cache
+        kmer_codon_table = _get_kmer_codon_table(genetic_code, cache)
+        step = cache * 3
+
+    return (
+        _translate_noncached(seq, sanitize=sanitize)
+        if (isinstance(seq, str) and not cache)
+        else (
+            _translate_cached(seq, sanitize=sanitize)
+            if isinstance(seq, str) and cache
+            else _vectorized_translate(seq, sanitize=sanitize)
+        )
+    )
 
 
 kmer_codon_tables = {}
