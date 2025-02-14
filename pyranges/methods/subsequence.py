6d891036a3de124fab0d4e1fe378d6a6fdc78061
endrebak.ada <endrebak85@gmail.com>
2025-02-14 12:53:22 +0100

    Not using indices 4

--- pyranges/methods/subsequence.py
+++ pyranges/methods/subsequence.py
@@ -34,7 +34,6 @@ def _subseq(
         chrs,
         df[START_COL].to_numpy(),
         df[END_COL].to_numpy(),
-        df.index.to_numpy(),
         (df[STRAND_COL] == FORWARD_STRAND).to_numpy()
         if (STRAND_COL in df and not force_plus_strand)
         else np.ones(len(df), dtype=bool),
@@ -43,7 +42,7 @@ def _subseq(
         force_plus_strand=force_plus_strand,
     )
 
-    outdf = df.loc[outidx]
+    outdf = df.take(outidx)
     outdf.loc[:, START_COL] = outstarts
     outdf.loc[:, END_COL] = outends
 
